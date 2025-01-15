f74921784af31ec61da216dcffba79889ac55058
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-01-15 12:17:01 -0300

    atualizando

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19870a8ae6ab87a0df8905fa8af77caad17aa0e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a9e26fbbc6d3a117b1a6a6dde213df02a51bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
